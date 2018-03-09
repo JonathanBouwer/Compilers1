c91914a2cb996a179f64f8b7ba8dfce0bdc4a169
Jonathan Bouwer <Jonathan.Bouwer1@gmail.com>
2018-03-09 11:28:56 +0200

    - Implemented pretty tree print

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,37 @@
 #include <string>
 #include "tree.h"
 
+#ifdef _WIN32
+#include <fcntl.h>
+#include <io.h>
+#endif
+
+void setupWindowsWideChar() {
+    #ifdef _WIN32
+    _setmode(_fileno(stdout), _O_WTEXT);
+    #endif
+}
+
 int main(int argc, char** argv) {
-    Tree<std::string> tree("Hello World!\n");
-    std::cout << tree;
+    setupWindowsWideChar();
+    
+    Tree<std::string> tree1("Element 1");
+    Tree<std::string> tree2("Element 2");
+    Tree<std::string> tree3("Element 3");
+    Tree<std::string> tree4("Element 4");
+    Tree<std::string> tree5("Element 5");
+    Tree<std::string> tree6("Element 6");
+    Tree<std::string> tree7("Element 7");
+    Tree<std::string> tree8("Element 8");
+    
+    tree1.addChild(tree2);
+    tree1.addChild(tree7);
+    tree2.addChild(tree3);
+    tree2.addChild(tree4);
+    tree2.addChild(tree6);
+    tree4.addChild(tree5);
+    tree7.addChild(tree8);
+    
+    std::wcout << tree1;
     return 0;
 }
--- src/tree.h
+++ src/tree.h
@@ -1,20 +1,44 @@
 #pragma once
 #include <vector>
 #include <iostream>
+#include <sstream>
+#include <string>
+
+/* Constant strings representing line shapes, uses wstring to support Box Drawing Characters
+** topInit = "├───"
+** topPre  = "│   "
+** botInit = "└───"
+** botPre  = "    "
+*/
+const std::wstring topInitPrefix = L"\u251C\u2500\u2500\u2500";
+const std::wstring topPrefix = L"\u2502    ";
+const std::wstring botInitPrefix = L"\u2514\u2500\u2500\u2500";
+const std::wstring botPrefix = L"    ";
+
+template <typename T>
+std::wstring toWString(T s) {
+    std::stringstream ss;
+    ss << s;
+    std::string treeValue = ss.str();
+    return std::wstring(treeValue.begin(), treeValue.end());
+}
 
 template <typename T>
 class Tree {
     private:
-        std::vector<Tree<T>> children;
+        std::vector<Tree<T>*> children;
+        Tree<T>* parent;
         T value;
     public:
         Tree() {}
         
-        Tree(T val) : value(val) {}
+        Tree(T val) : value(val), parent(NULL) {}
         
-        Tree(const Tree& other) : value(other.value), children(other.children) {}
+        Tree(const Tree& other) : value(other.value), 
+                                  children(other.children) {}
         
-        Tree(Tree&& other) : value(std::move(other.value)), children(std::move(other.children)) {}
+        Tree(Tree&& other) : value(std::move(other.value)), 
+                             children(std::move(other.children)) {}
         
         Tree& operator=(const Tree& other) {
             value = other.value;
@@ -28,8 +52,29 @@ class Tree {
         
         ~Tree() {}
         
-        friend std::ostream& operator<<(std::ostream& out, const Tree<T>& tree) {
-            out << tree.value;
+        void addChild(Tree& child) {
+            children.push_back(&child);
+        }
+        
+        friend std::wostream& operator<<(std::wostream& out, const Tree<T>& tree) {
+            out << prettyPrint(tree);
             return out;
+        }
+        
+        friend std::wstring prettyPrint(const Tree<T>& tree, 
+                                        std::wstring initialPrefix = L"", 
+                                        std::wstring prefix = L"") {
+            std::wstringstream output;
+            output << initialPrefix << toWString<T>(tree.value)  << L"\n";
+            if (tree.children.size() == 0) return output.str();
+            
+            int lastChildIndex = tree.children.size() - 1;            
+            for (int i = 0; i < lastChildIndex; i++) {
+                Tree<T> *child = tree.children[i];
+                output << prettyPrint(*child, prefix + topInitPrefix, prefix + topPrefix);
+            }
+            Tree<T> *lastChild = tree.children[lastChildIndex];
+            output << prettyPrint(*lastChild, prefix + botInitPrefix, prefix + botPrefix);
+            return output.str();
         }
 };
