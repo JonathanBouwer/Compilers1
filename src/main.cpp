b8586220464d8d5cf5618ba0256ec2a2bba0ce3f
Dylan S <dylansims3757@hotmail.co.za>
2018-03-12 08:51:47 +0200

    Adding changes to main to call print tree. Print tree is only used as tester ATM

--- src/main.cpp
+++ src/main.cpp
@@ -13,26 +13,33 @@ void setupWindowsWideChar() {
     #endif
 }
 
+void printPrettyTestTree() {
+	Tree<std::string> tree1("Element 1");
+	Tree<std::string> tree2("Element 2");
+	Tree<std::string> tree3("Element 3");
+	Tree<std::string> tree4("Element 4");
+	Tree<std::string> tree5("Element 5");
+	Tree<std::string> tree6("Element 6");
+	Tree<std::string> tree7("Element 7");
+	Tree<std::string> tree8("Element 8");
+
+	tree1.addChild(tree2);
+	tree1.addChild(tree7);
+	tree2.addChild(tree3);
+	tree2.addChild(tree4);
+	tree2.addChild(tree6);
+	tree4.addChild(tree5);
+	tree7.addChild(tree8);
+
+	std::wcout << tree1;
+}
+
 int main(int argc, char** argv) {
-    setupWindowsWideChar();
-    
-    Tree<std::string> tree1("Element 1");
-    Tree<std::string> tree2("Element 2");
-    Tree<std::string> tree3("Element 3");
-    Tree<std::string> tree4("Element 4");
-    Tree<std::string> tree5("Element 5");
-    Tree<std::string> tree6("Element 6");
-    Tree<std::string> tree7("Element 7");
-    Tree<std::string> tree8("Element 8");
-    
-    tree1.addChild(tree2);
-    tree1.addChild(tree7);
-    tree2.addChild(tree3);
-    tree2.addChild(tree4);
-    tree2.addChild(tree6);
-    tree4.addChild(tree5);
-    tree7.addChild(tree8);
-    
-    std::wcout << tree1;
+	setupWindowsWideChar();
+
+	//call to function to test pretty tree printing
+	printPrettyTestTree();
+
     return 0;
 }
+
