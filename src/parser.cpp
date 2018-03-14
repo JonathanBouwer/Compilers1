a22e0688647e68ca8d91a3945378b598dc623802
Dylan S <dylansims3757@hotmail.co.za>
2018-03-14 14:57:08 +0200

    Added some code to play with parser. Started with functions

--- src/parser.cpp
+++ src/parser.cpp
@@ -4,4 +4,22 @@
 #include <stack>
 using namespace std
 
+Tree lib(stack<Token> &allTokens){
+	tokenType topItem = allTokens.top();
+	if (topItem.type == tokenType.KEYWORD_PRETEXT)
+	Tree parseTree<Token>(topItem);
+	allTokens.pop();
 
+}
+
+&Tree funcions(stack<Token> &allTokens){
+
+}
+
+&Tree statements(stack<Token> &allTokens){
+
+}
+
+&Tree expressions(stack<Token> &allTokens){
+
+}
