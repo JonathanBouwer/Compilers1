78d71a33eafe404212a9380b24c6070b626a5ce6
PieterVDWalt <pieter072@gmail.com>
2018-03-14 16:54:33 +0200

    lib function started, Dylan to add throw

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,25 +1,35 @@
 #include <string>
+#include<iostream>
 #include "tree.h"
 #include "tokenType.h"
 #include <stack>
-using namespace std
+#include <vector>
+using namespace std;
 
-Tree lib(stack<Token> &allTokens){
-	tokenType topItem = allTokens.top();
-	if (topItem.type == tokenType.KEYWORD_PRETEXT)
-	Tree parseTree<Token>(topItem);
-	allTokens.pop();
+class Parser{
+	Parser(){}
 
-}
+	Tree lib(stack<Token> &allTokens){
+		Token topItem = allTokens.top();
+		if (topItem.type == tokenType.KEYWORD_PRETEXT) {
+			Tree parseTree<Token>(topItem);
+			allTokens.pop();
+			parseTree.addChild(functions(allTokens);
+		} else {
+			printf("Failure on character %s at row %d, column %d. Expected the Pretext keyword, I did. This is why you fail. \n", topItem.literal, topItem.row, topItem.column);
+			exit(0);
+		}
+	}
 
-&Tree funcions(stack<Token> &allTokens){
+	&vector<Tree> funcions(stack<Token> &allTokens){
 
-}
+	}
 
-&Tree statements(stack<Token> &allTokens){
+	&vector<Tree> statements(stack<Token> &allTokens){
 
-}
+	}
 
-&Tree expressions(stack<Token> &allTokens){
+	&vector<Tree> expressions(stack<Token> &allTokens){
 
+	}
 }
