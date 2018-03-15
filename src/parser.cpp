1b32e744f96a08ca360223f20568d55be6f3ebfa
Dylan S <dylansims3757@hotmail.co.za>
2018-03-15 09:16:41 +0200

    Adding template to make comments

--- src/parser.cpp
+++ src/parser.cpp
@@ -9,27 +9,28 @@ using namespace std;
 class Parser{
 	Parser(){}
 
-	Tree lib(stack<Token> &allTokens){
+	Tree<Token> lib(stack<Token> &allTokens){
 		Token topItem = allTokens.top();
 		if (topItem.type == tokenType.KEYWORD_PRETEXT) {
-			Tree parseTree<Token>(topItem);
+			Tree<Token> parseTree(topItem);
 			allTokens.pop();
-			parseTree.addChild(functions(allTokens);
+			parseTree.addChild(functions(allTokens));
 		} else {
 			printf("Failure on character %s at row %d, column %d. Expected the Pretext keyword, I did. This is why you fail. \n", topItem.literal, topItem.row, topItem.column);
 			exit(0);
 		}
 	}
 
-	&vector<Tree> funcions(stack<Token> &allTokens){
+//
+	Tree<Token> funcions(stack<Token> &allTokens){
 
 	}
 
-	&vector<Tree> statements(stack<Token> &allTokens){
+	Tree<Token> statements(stack<Token> &allTokens){
 
 	}
 
-	&vector<Tree> expressions(stack<Token> &allTokens){
+	Tree<Token> expressions(stack<Token> &allTokens){
 
 	}
 }
