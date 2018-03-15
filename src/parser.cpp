725245dfa23c2cf8b6a489694cbedc52f186b157
PieterVDWalt <pieter072@gmail.com>
2018-03-15 09:35:51 +0200

    added format of lib, expB and expNB

--- src/parser.cpp
+++ src/parser.cpp
@@ -8,7 +8,9 @@ using namespace std;
 
 class Parser{
 	Parser(){}
-
+/*
+lib -> KEYWORD_PRETEXT . Function* . KEYWORD_EOF
+*/
 	Tree<Token> lib(stack<Token> &allTokens){
 		Token topItem = allTokens.top();
 		if (topItem.type == tokenType.KEYWORD_PRETEXT) {
@@ -29,11 +31,30 @@ class Parser{
 	Tree<Token> statements(stack<Token> &allTokens){
 
 	}
-
+/*
+ExpB -> OPERATOR_PLUS . ExpNB |
+		OPERATOR_TIMES . ExpNB |
+		OPERATOR_MINUS . ExpNB |
+		OPERATOR_DIVIDE . ExpNB |
+		OPERATOR_MOD . ExpNB |
+		OPERATOR_POW . ExpNB |
+		OPERATOR_OR . ExpNB |
+		OPERATOR_AND . ExpNB |
+		OPERATOR_IS . ExpNB |
+		e
+*/
 	Tree<Token> expressionBinary(stack<Token> &allTokens){
 
 	}
-
+/*
+ExpNB -> IDENTIFIER_VARIABLE . ExpB |
+		 LITERAL_INTEGER . ExpB |
+		 LITERAL_STRING . ExpB |
+		 LITERAL_CHAR . ExpB |
+		 SEPARATOR_LEFT_PAREN . ExpNB . SEPARATOR_RIGHT_PAREN |
+		 KEYWORD_EXECUTE . IDENTIFIER_FUNCTION . SEPARATOR_LEFT_PAREN . ExpNB . SEPARATOR_RIGHT_PAREN |
+		 SEPARATOR_LEFT_PAREN . ExpNB . SEPARATOR_RIGHT_PAREN
+*/
 	Tree<Token> expressionNonBinary(stack<Token> &allTokens){
 
 	}
