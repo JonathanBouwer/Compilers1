8224f7209229cb4d016382389d650138929bfe4b
PieterVDWalt <pieter072@gmail.com>
2018-03-17 17:51:29 +0200

    finished - without testing - the lib, expB and expNB functions

--- src/parser.cpp
+++ src/parser.cpp
@@ -92,29 +92,39 @@ class Parser{
 /*
 lib -> KEYWORD_PRETEXT . Function* . KEYWORD_EOF
 */
-Tree<Token> lib(stack<Token> &allTokens)   {
+void lib(stack<Token> &allTokens, Tree<Token> &parentTree)   {
 
-	Tree<Token> retTree();
+	queue<Tree<Token> > usedTrees;
 	Token top = allTokens.top();
 	if (top.type = TokenType.KEYWORD_PRETEXT) {
-		retTree.addChild(Tree<Token>(top));
+		Tree<Token> begin(top.value, top.type, top.row, top.column);
+		usedTrees.push(begin);
 		allTokens.pop();
 	} else {
 		throw string("Failure on character %s at row %d, column %d.
 		Expected the Pretext keyword, I did. This is why you fail. \n",
-		 top.literal, top.row, top.column);
+		 top.value, top.row, top.column);
 	}
 
 	while (allTokens.top() != TokenType.KEYWORD_EOF && allTokens.size() > 0) {
-		retTree.addChild(functions(&allTokens));
+		Tree<Token> functionTree("FUNCTION",TokenType.FUNCTION, 0,0);//example of token constructor. row + column doesnt matter in this case because it's just a keyword we're adding
+		functions(allTokens, functionTree);
+		usedTrees.push(functionTree);
 	}
 
 	if (allTokens.top() == TokenType.KEYWORD_EOF) {
+		Token top = allTokens.top();
 		allTokens.pop();
-		return retTree;
+		Tree<Token> end(top.value, top.type, top.row, top.column);
+		usedTrees.push(end);
 	} else {
 		throw string("Failure: Expected the End of File keyword, I did.
 		 			This is why you fail. \n");
+	}
+
+	while (usedTree.size() > 0) {
+		parentTree.addChild(usedTrees.front());
+		usedTrees.pop();
 	}
 }
 
@@ -147,8 +157,8 @@ Functions -> Keyword_Force . Identifier_Function . Seperator_Left_Paren . Idenif
 					//Tree<Token> tempTree = Statements(allTokens);
 					//funcTree.AddChild(tempTree);
 					//usedTokens.push(allTokens.top());
-					// 
-					//TODO Here is where we have to add the placeholder statement call 
+					//
+					//TODO Here is where we have to add the placeholder statement call
 					//
 					Tree<Token> statementConverter(/* Change this to be placeholder */allTokens.top());
 					//allTokens.pop();
@@ -181,7 +191,7 @@ Functions -> Keyword_Force . Identifier_Function . Seperator_Left_Paren . Idenif
 		throw string("Failure at character %s, row %d, column %d.
 							This is why you fail",
 							top.literal, top.row, top.column);
-		
+
 	}
 /*
 Statements->Keyword_Then . Statement* . Keyword_You_Must |
@@ -204,8 +214,8 @@ Statements->Keyword_Then . Statement* . Keyword_You_Must |
 					if (FUNCTIONTOKEN[outerloop][innerLoop] == Token.STATEMENT){
 						while (allTokens.size() != 0 && allTokens.top() == Token.STATEMENT){
 							//funcTree.AddChild(Tree<Token>(allTokens.top()));
-							// 
-							//TODO Here is where we have to add the placeholder statement call 
+							//
+							//TODO Here is where we have to add the placeholder statement call
 							//
 							Tree<Token> statementConverter(/* Change this to be placeholder */allTokens.top());
 							//allTokens.pop();
@@ -225,8 +235,8 @@ Statements->Keyword_Then . Statement* . Keyword_You_Must |
 					continue;
 				}else if (allTokens.size() != 0 && STATEMENTOKEN[outerloop][innerLoop] == Token.EXPRESSIONNB){
 					//usedTokens.push(allTokens.top());
-					// 
-					//TODO Here is where we have to add the placeholder expression call 
+					//
+					//TODO Here is where we have to add the placeholder expression call
 					//
 					Tree<string> exprNB(/* Change this to be placeholder */allTokens.top());
 					//allTokens.pop();
@@ -259,8 +269,9 @@ Statements->Keyword_Then . Statement* . Keyword_You_Must |
 		throw string("Failure at character %s, row %d, column %d.
 							This is why you fail",
 							top.literal, top.row, top.column);
-		
+
 	}
+}
 /*
 ExpB -> OPERATOR_PLUS . ExpNB |
 		OPERATOR_TIMES . ExpNB |
@@ -273,28 +284,38 @@ ExpB -> OPERATOR_PLUS . ExpNB |
 		OPERATOR_IS . ExpNB |
 		e
 */
-	Tree<Token> expressionBinary(stack<Token> &allTokens){
-		Tree<Token> expB();
-		Token top; //not sure how token will be initialized
-		for (int outerLoop = 0; outerLoop < EXPBTOKEN.size(); outerLoop++) {
-			for (int innerLoop = 0; innerLoop < EXPBTOKEN.[outerLoop].size(); innerLoop++) {
-				if (allTokens.size() > 0 && EXPBTOKEN[outerloop][innerloop] == TokenType.EXPRESSIONNB) {
-					expB.addChild(expressionNonBinary(&allTokens));
-					return expB;
-				} else {
-					top = allTokens.top();
-					if (top.type == EXPBTOKEN[outerloop][innerloop]) {
-						allTokens.pop();
-						expB.addChild(Tree<Token>(top));
-					} else {
-						break; //no need for used stack here, since expB only 2 tokens long
-					}
+void expressionBinary(stack<Token> &allTokens, Tree<Token> &parentTree){
+	queue<Tree<Token> > usedTrees;
+	for (int outerLoop = 0; outerLoop < EXPBTOKEN.size(); outerLoop++) {
+		for (int innerLoop = 0; innerLoop < EXPBTOKEN.[outerLoop].size(); innerLoop++) {
+			if (allTokens.size() > 0 && EXPBTOKEN[outerloop][innerloop] == TokenType.EXPRESSIONNB) {
+				Tree<Token> expressionNBTree("EXPNB",TokenType.EXPRESSIONNB,0,0);
+				expressionNonBinary(allTokens, expressionNBTree);
+				usedTrees.push(expressionNBTree);
+				while (usedTree.size() > 0) { //because expB always ends on ExpNB
+					parentTree.addChild(usedTrees.front());
+					usedTrees.pop();
 				}
-			} //didnt add a throw message here, because expB can also just be empty, i.e. next character could be a large variety of things.
+				return;
+			} else {
+				top = allTokens.top();
+				if (top.type == EXPBTOKEN[outerloop][innerloop]) {
+					allTokens.pop();
+					Tree<Token> Operator(top.value, top.type, top.row, top.column);
+					usedTrees.push(Operator);
+				} else {
+					break; //no need for used stack here, since expB only 2 tokens long
+				}
+			}
+		} //didnt add a throw message here, because expB can also just be empty, i.e. next character could be a large variety of things.
 
-		}
-		return expB;
 	}
+	while (usedTree.size() > 0) {
+		parentTree.addChild(usedTrees.front());
+		usedTrees.pop();
+	}
+	return;
+}
 /*
 ExpNB -> IDENTIFIER_VARIABLE . ExpB |
 		 LITERAL_INTEGER . ExpB |
@@ -303,65 +324,92 @@ ExpNB -> IDENTIFIER_VARIABLE . ExpB |
 		 SEPARATOR_LEFT_PAREN . ExpNB . SEPARATOR_RIGHT_PAREN |
 		 KEYWORD_EXECUTE . IDENTIFIER_FUNCTION . SEPARATOR_LEFT_PAREN . Params . SEPARATOR_RIGHT_PAREN |
 */
-	Tree<Token> expressionNonBinary(stack<Token> &allTokens){
-		Tree<Token> expNB();
-		stack<Token> usedTokens;
-		Token top; //not sure how token will be initialized
+void expressionNonBinary(stack<Token> &allTokens, Tree<Token> &parentTree){
+	queue<Tree<Token> > usedTrees;
+	stack<Token> usedTokens;
 
-		for (int outerLoop = 0; outerLoop < EXPNBTOKEN.size(); outerloop++) {
-			for (int innerLoop = 0; innerLoop < EXPNBTOKEN[outerloop].size();innerloop++) {
-				if (allTokens.size() > 0 && EXPBTOKEN[outerloop][innerloop] == TokenType.EXPRESSIONB) {
-					expNB.addChild(expressionBinary(&allTokens));
-					return expNB; //because ExpB is always at the end of ExpNB
-				} else if (allTokens.size() > 0 && EXPBTOKEN[outerloop][innerloop] == TokenType.EXPRESSIONNB) {
-					expNB.addChild(expressionNonBinary(&allTokens));
-				} else if (allTokens.size() > 0 && EXPBTOKEN[outerloop][innerloop] == TokenType.PARAMS) { //checking parameters
-					if (allTokens.top() != TokenType.SEPERATOR_RIGHT_PAREN) {
-						expNB.addChild(expressionNonBinary(&allTokens));
-						while (allTokens.top() != TokenType.SEPERATOR_RIGHT_PAREN) {
-							top = allTokens.top();
-							if (top.type == TokenType.OPERATOR_COMMA) {
-								allTokens.pop();
-								expNB.addChild(expressionNonBinary(&allTokens));
-							} else {
-								throw string ("Failure at character %d, row %d, column %d.
-								Expected a comma or right parenthesis, did I. This is why you fail",
-								top.literal, top.row, top.column);
-							}
+	for (int outerLoop = 0; outerLoop < EXPNBTOKEN.size(); outerloop++) {
+		for (int innerLoop = 0; innerLoop < EXPNBTOKEN[outerloop].size();innerloop++) {
+			if (allTokens.size() > 0 && EXPBTOKEN[outerloop][innerloop] == TokenType.EXPRESSIONB) {
+				Tree<Token> expressionBTree("EXPB",TokenType.EXPRESSIONB,0,0);
+				expressionBinary(allTokens,expressionBTree);
+				usedTrees.push(expressionBTree);
+				while (usedTree.size() > 0) { //because ExpB is always last token in ExpNB
+					parentTree.addChild(usedTrees.front());
+					usedTrees.pop();
+				}
+				return;
+			} else if (allTokens.size() > 0 && EXPBTOKEN[outerloop][innerloop] == TokenType.EXPRESSIONNB) {
+				Tree<Token> expressionNBTree("EXPNB",TokenType.EXPRESSIONNB,0,0);
+				expressionNonBinary(allTokens, expressionNBTree);
+				usedTrees.push(expressionNBTree);
+			} else if (allTokens.size() > 0 && EXPBTOKEN[outerloop][innerloop] == TokenType.PARAMS) { //checking parameters
+				if (allTokens.top() != TokenType.SEPERATOR_RIGHT_PAREN) {
+					Tree<Token> paramTree("PARAMS",TokenType.PARAMS,0,0);
+					expressionNonBinary(allTokens,paramTree);
+					usedTrees.push(paramTree);
+					while (allTokens.top() != TokenType.SEPERATOR_RIGHT_PAREN) {
+						top = allTokens.top();
+						if (top.type == TokenType.OPERATOR_COMMA) {
+							usedTokens.push(top);
+							allTokens.pop();
+							Tree<Token> paramTreeMore("PARAMS",TokenType.PARAMS,0,0);
+							expressionNonBinary(allTokens,paramTreeMore);
+							usedTrees.push(paramTreeMore);
+						} else {
+							throw string ("Failure at character %d, row %d, column %d.
+							Expected a comma or right parenthesis, did I. This is why you fail",
+							top.value, top.row, top.column);
 						}
-						top = allTokens.top();
-						allTokens.pop();
-						expNB.addChild(Tree<Token>(top));
-						return expNB; //because in expNB, after params it's only right paren and then end
-					} else {
-						top = allTokens.top();
-						allTokens.pop();
-						expNB.addChild(Tree<Token>(top));
-						return expNB; //because in expNB, after params it's only right paren and then end
 					}
+					top = allTokens.top();
+					usedTokens.push(top);
+					allTokens.pop();
+					while (usedTree.size() > 0) { //because right paren is always at end of ExpNB
+						parentTree.addChild(usedTrees.front());
+						usedTrees.pop();
+					}
+					return;
 				} else {
 					top = allTokens.top();
-					if (top == EXPNBTOKEN[outerloop][innerloop]) {
-						expNB.addChild(Tree<Token>(top));
-						usedTokens.push(top);
-						allTokens.pop();
+					usedTokens.push(top);
+					allTokens.pop();
+					while (usedTree.size() > 0) { //because right paren is always at end of ExpNB
+						parentTree.addChild(usedTrees.front());
+						usedTrees.pop();
+					}
+					return;
+
+				}
+			} else {
+				top = allTokens.top();
+				if (top == EXPNBTOKEN[outerloop][innerloop]) {
+					Tree<Token> temp(top.value, top.type,0,0);
+					usedTrees.push(temp);
+					usedTokens.push(top);
+					allTokens.pop();
+				} else {
+					if (outerloop == EXPNBTOKEN.size()-1) {
+						throw string ("Failure at character %s, row %d, column %d.
+						Matches a non-binary expression, it does not. This is why you fail",
+						top.value, top.row, top.column);
 					} else {
-						if (outerloop == EXPNBTOKEN.size()-1) {
-							throw string ("Failure at character %s, row %d, column %d.
-							Matches a non-binary expression, it does not. This is why you fail",
-							top.literal, top.row, top.column);
-						} else {
-							while (usedTokens.size() != 0) {
-								allTokens.push(usedTokens.top());
-								usedTokens.pop();
-							}
-						}
+						break;
 					}
 				}
 			}
 		}
-		return expNB;
-
+		while (usedTokens.size() != 0) {
+			allTokens.push(usedTokens.top());
+			usedTokens.pop();
+		}
 	}
+	while (usedTree.size() > 0) {
+		parentTree.addChild(usedTrees.front());
+		usedTrees.pop();
+	}
+	return;
+
+}
 
 }
